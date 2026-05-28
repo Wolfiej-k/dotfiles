d3f559e8cf8474b9ece15d19f8ccd6727577469c
Taj Jethwani-Keyser <wolfiejetkey@gmail.com>
2026-05-27 23:14:00 -0400

    remove nvim plugins for latex and lean

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -57,65 +57,6 @@ return {
         dependencies = { "rafamadriz/friendly-snippets" },
         config = function()
             require("luasnip.loaders.from_vscode").lazy_load()
-
-            local ls = require("luasnip")
-            local s = ls.snippet
-            local i = ls.insert_node
-            local fmta = require("luasnip.extras.fmt").fmta
-
-            ls.config.set_config({
-                enable_autosnippets = true,
-                update_events = "TextChanged,TextChangedI",
-            })
-
-            local function in_mathmode()
-                return vim.fn['vimtex#syntax#in_mathzone']() == 1
-            end
-
-            local function no_backslash_before()
-                return function(line_to_cursor)
-                    return not line_to_cursor:match("\\_$") and not line_to_cursor:match("\\%^$")
-                end
-            end
-
-            local function no_curly_after()
-                return vim.fn.getline('.'):sub(vim.fn.col('.'), vim.fn.col('.')) ~= '{'
-            end
-
-            local function auto_trigger()
-                return in_mathmode() and no_backslash_before() and no_curly_after()
-            end
-
-            ls.add_snippets("tex", {
-                s("align", fmta(
-                    [[
-\begin{align*}
-    <>
-\end{align*}
-]],
-                    { i(1) }
-                )),
-                s("solution", fmta(
-                    [[
-\begin{solution}
-    <>
-\end{solution}
-]],
-                    { i(1) }
-                )),
-                s("left", fmta([[\left(<>\right)]], { i(1) })),
-                s("frac", fmta([[\frac{<>}{<>}]], { i(1), i(2) })),
-                s("sum", fmta([[\sum_{<>}^{<>}]], { i(1), i(2) })),
-                s("prod", fmta([[\prod{<>}^{<>}]], { i(1), i(2) })),
-                s("rm", fmta([[\mathrm{<>}]], { i(1) })),
-                s({ trig = "$", snippetType = "autosnippet" },
-                    fmta([[$<>$]], { i(1) })
-                ),
-                s({ trig = "_", wordTrig = false, snippetType = "autosnippet" },
-                    fmta([[_{<>}]], { i(1) }), { condition = auto_trigger }),
-                s({ trig = "^", wordTrig = false, snippetType = "autosnippet" },
-                    fmta([[^{<>}]], { i(1) }), { condition = auto_trigger }),
-            })
         end,
     },
 
